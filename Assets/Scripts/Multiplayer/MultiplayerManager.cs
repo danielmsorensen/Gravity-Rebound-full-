257c5339cee5ea0936d54813bb26c9ecf681e28c
Daniel Sorensen <daniel.m.sorensen@gmail.com>
2017-11-29 22:10:46 +0000

    Add Unity Project

---
+++ Assets/Scripts/Multiplayer/MultiplayerManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GravitySwitcher))]
+[RequireComponent(typeof(ObstacleSpawner))]
+public class MultiplayerManager : Game {
+
+    public string playerPrefabName;
+
+    public Color localPlayerColour;
+
+    GravitySwitcher gravity;
+    ObstacleSpawner spawner;
+
+    protected override void Awake() {
+        base.Awake();
+
+        Time.timeScale = 0;
+
+        gravity = GetComponent<GravitySwitcher>();
+        spawner = GetComponent<ObstacleSpawner>();
+
+        gravity.enabled = false;
+        spawner.enabled = false;
+
+        PhotonNetwork.ConnectUsingSettings("Gravity Rebound " + Application.version);
+    }
+
+    void OnJoinedLobby() {
+        Debug.Log("Joined Lobby");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed(object[] codeAndMsg) {
+        Debug.Log("Creating Room");
+        PhotonNetwork.CreateRoom("");
+    }
+
+    void OnJoinedRoom() {
+        Debug.Log("Joined Room");
+
+        NetworkPlayer player = PhotonNetwork.Instantiate(playerPrefabName, Vector3.zero, Quaternion.identity, 0).GetComponent<NetworkPlayer>();
+
+        player.enabled = true;
+        player.gameObject.layer = 0;
+
+        player.SetColour(localPlayerColour);
+
+        gravity.target = spawner.target = player;
+        CameraManager.main.target = player.transform;
+
+        Time.timeScale = 1;
+
+        gravity.enabled = true;
+        spawner.enabled = true;
+    }
+
+    void OnGUI() {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+}
---
+++ Assets/Scripts/Utilities/MouseText.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MouseText : MonoBehaviour {
+
+    public Selectable[] elements;
+    [Space]
+    public Transform message;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        foreach (Selectable e in elements) {
+            if (MouseOver(e.GetComponent<RectTransform>()) && e.gameObject.activeInHierarchy) {
+                message.gameObject.SetActive(true);
+                message.GetComponent<RectTransform>().position = Input.mousePosition;
+
+                return;
+            }
+        }
+
+        message.gameObject.SetActive(false);
+    }
+
+    public static bool MouseOver(RectTransform transform) {
+        Vector2 mousePosition = Input.mousePosition;
+        Vector3[] worldCorners = new Vector3[4];
+        transform.GetWorldCorners(worldCorners);
+
+        // Corners:
+        // 1 2
+        // 0 3
+
+        if (mousePosition.x >= worldCorners[0].x && mousePosition.x < worldCorners[2].x && mousePosition.y >= worldCorners[0].y && mousePosition.y < worldCorners[2].y) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
